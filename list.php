8b0cdf48f515f67b0d564425ff0acdd341bfce60
Lidia Mejia <lidiamejia29@yahoo.com>
2020-08-09 20:55:19 -0600

    Estilo Listado de Familias

---
+++ list.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/estilo.css"/>
+    <script src="https://kit.fontawesome.com/b17457e142.js" crossorigin="anonymous"></script>
+
+    <title>Lista de Familias</title>
+</head>
+
+<body>
+
+    <header class="p-3 mr-3 mb-5">Parroquia Cristo Resucitado</header>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="mb-4">Listado de Familias</h1>
+
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Familia</th>
+                                <th scope="col">Celular</th>
+                                <th scope="col">Correo Electrónico</th>
+                                <th scope="col">Capilla</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Prueba</td>
+                                <td>12345678</td>
+                                <td>prueba@yahoo.com</td>
+                                <td>Las Mercedes</td>
+                                <td><i class="fas fa-trash" id="trash"></i></td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Prueba</td>
+                                <td>12345678</td>
+                                <td>prueba@yahoo.com</td>
+                                <td>Las Mercedes</td>
+                                <td><i class="fas fa-trash" id="trash"></i></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="p-3"></footer>
+
+</body>
+
+</html>
